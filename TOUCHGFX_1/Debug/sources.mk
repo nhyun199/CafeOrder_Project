5a06be08e57a7c586408c406c92912e392585bed
nhyun199 <shgus1692@gmail.com>
2024-11-22 17:06:03 +0900

    touchgfx
    touch screen
    control gpio with ui button

---
+++ TOUCHGFX_1/Debug/sources.mk
@@ -0,0 +1,88 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+C++M_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+CCM_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+CXXM_SRCS := 
+CYCLO_FILES := 
+OBJDUMP_LIST := 
+CCM_DEPS := 
+C_UPPER_DEPS := 
+S_DEPS := 
+CXXM_DEPS := 
+C_DEPS := 
+CC_DEPS := 
+SIZE_OUTPUT := 
+C++_DEPS := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+CXX_DEPS := 
+MAP_FILES := 
+S_UPPER_DEPS := 
+C++M_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/BSP/Components/ft5336 \
+Drivers/BSP/STM32746G-Discovery \
+Drivers/STM32F7xx_HAL_Driver/Src \
+FATFS/App \
+FATFS/Target \
+Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src \
+Middlewares/ST/STM32_USB_Host_Library/Core/Src \
+Middlewares/Third_Party/FatFs/src \
+Middlewares/Third_Party/FatFs/src/option \
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS \
+Middlewares/Third_Party/FreeRTOS/Source \
+Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1 \
+Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang \
+TouchGFX/App \
+TouchGFX/build/MINGW32_NT-6.2/generated/fonts/src \
+TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/common \
+TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/screen1_screen \
+TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/screen_screen \
+TouchGFX/build/MINGW32_NT-6.2/generated/images/src \
+TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__generated \
+TouchGFX/build/MINGW32_NT-6.2/generated/simulator/src \
+TouchGFX/build/MINGW32_NT-6.2/generated/simulator/src/video \
+TouchGFX/build/MINGW32_NT-6.2/generated/texts/src \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/common \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/model \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/screen1_screen \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/screen_screen \
+TouchGFX/build/MINGW32_NT-6.2/simulator \
+TouchGFX/build/MINGW32_NT-6.2/touchgfx/framework/source/platform/driver/touch \
+TouchGFX/build/MINGW32_NT-6.2/touchgfx/framework/source/platform/hal/simulator/sdl2 \
+TouchGFX/generated/fonts/src \
+TouchGFX/generated/gui_generated/src/common \
+TouchGFX/generated/gui_generated/src/screen1_screen \
+TouchGFX/generated/gui_generated/src/screen_screen \
+TouchGFX/generated/images/src \
+TouchGFX/generated/images/src/__generated \
+TouchGFX/generated/texts/src \
+TouchGFX/gui/src/common \
+TouchGFX/gui/src/model \
+TouchGFX/gui/src/screen1_screen \
+TouchGFX/gui/src/screen_screen \
+TouchGFX/target \
+TouchGFX/target/generated \
+USB_HOST/App \
+USB_HOST/Target \
+
